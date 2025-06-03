8a4c5a1a8aef3b958b6a6f4afb131ac7226dedb9
smile <mad024039@gmail.com>
2025-06-03 19:26:05 +0800

    style(emp, interceptor, config): 将4空格缩进改为2空格缩进
    - EmployeeController.java
    - EmployeeService.java
    - EmployeeServiceImpl.java
    - EmployeeMapper.java
    - JwtTokenAdminInterceptor.java
    - WebMvcConfiguration.java

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -7,12 +7,12 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface EmployeeMapper {
 
-    /**
-     * 根据用户名查询员工
-     * @param username
-     * @return
-     */
-    @Select("select * from employee where username = #{username}")
-    Employee getByUsername(String username);
+  /**
+   * 根据用户名查询员工
+   * @param username
+   * @return
+   */
+  @Select("select * from employee where username = #{username}")
+  Employee getByUsername(String username);
 
 }
