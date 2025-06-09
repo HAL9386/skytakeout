b6bdccac8b3cdb3fc9f8c66c5b3f370bc655ff1f
smile <mad024039@gmail.com>
2025-06-09 21:07:00 +0800

    feat(utils): 配置阿里云存储服务

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,23 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自定义Oss配置类
+ */
+@Slf4j
+@Configuration
+public class OssConfiguration {
+  public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+    log.info("开始创建阿里云文件上传工具类对象：{}", aliOssProperties);
+    return AliOssUtil.builder()
+      .endpoint(aliOssProperties.getEndpoint())
+      .accessKeyId(aliOssProperties.getAccessKeyId())
+      .accessKeySecret(aliOssProperties.getAccessKeySecret())
+      .bucketName(aliOssProperties.getBucketName())
+      .build();
+  }
+}
