3a5e65e611f787858dabeadbdfa61ac620c8aae3
smile <mad024039@gmail.com>
2025-06-12 02:22:08 +0800

    fix(dish): 修复新增菜品失败
    新增菜品需要插入菜品口味表的相应数据
    而菜品口味关联了菜品，插入口味数据时需要
    相应菜品id。所以在插入菜品数据后需要获取到
    菜品的id，并设置到口味数据中

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -3,7 +3,6 @@ package com.sky.mapper;
 import com.sky.annotation.AutoFill;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -21,12 +20,10 @@ public interface DishMapper {
 
 
   /**
-   * 新增菜品
+   * 插入菜品数据
    *
    * @param dish 菜品对象
    */
   @AutoFill(value = OperationType.INSERT)
-  @Insert("insert into dish(name, category_id, price, image, description, status, create_time, update_time, create_user, update_user) " +
-    "values(#{name}, #{categoryId}, #{price}, #{image}, #{description}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
   void insert(Dish dish);
 }
