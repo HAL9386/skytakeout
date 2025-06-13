4d74c137d371ae256be510541fa27ececac6188c
smile <mad024039@gmail.com>
2025-06-13 23:32:52 +0800

    feat(dish): 分页查询菜品信息

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 
 public interface DishService {
   /**
@@ -9,4 +12,12 @@ public interface DishService {
    * @param dishDTO 菜品基本信息及对应的口味数据
    */
   void saveWithFlavor(DishDTO dishDTO);
+
+  /**
+   * 菜品分页查询
+   *
+   * @param dishPageQueryDTO 分页查询条件
+   * @return 分页查询结果
+   */
+  PageResult<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
