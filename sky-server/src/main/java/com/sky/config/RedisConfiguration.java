f0c0c665c1fee9a5cbeae9847c03406b9c8e806d
smile <mad024039@gmail.com>
2025-07-03 16:39:48 +0800

    feat(config): 为redisTemplate配置hash的序列化器

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -18,6 +18,8 @@ public class RedisConfiguration {
     redisTemplate.setConnectionFactory(redisConnectionFactory);
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
     return redisTemplate;
   }
 }
