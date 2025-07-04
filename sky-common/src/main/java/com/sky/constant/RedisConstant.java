0c1a1d9d9e077ea179e4e44c63b2edb10bd8f847
smile <mad024039@gmail.com>
2025-07-04 14:39:37 +0800

    fix(dish): 更改菜品缓存的键
    "dishWithCatId_" -> "dishListByCategoryId:"

--- sky-common/src/main/java/com/sky/constant/RedisConstant.java
+++ sky-common/src/main/java/com/sky/constant/RedisConstant.java
@@ -1,6 +1,6 @@
 package com.sky.constant;
 
 public class RedisConstant {
-  public static final String DISH_LIST_KEY_PREFIX = "dishWithCatId_";
+  public static final String DISH_LIST_KEY_PREFIX = "dishListByCategoryId:";
   public static final String SETMEAL_LIST_KEY = "setmealListByCategoryId";
 }
